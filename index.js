a54d6f620a254caa40bf217177d522dbc6d4df0e
Gabriel Tedesco Pastori <gabrielpastori1@hotmail.com>
2020-10-15 22:38:10 -0300

    Initial

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -7,9 +8,18 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.use('/src',express.static('public'));
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+  });
+  socket.on('emergencial', function(msg){
+    io.emit('meet', socket.id);
+    console.log(msg);
+    setTimeout(()=>{
+      io.emit('new-meet',true);
+    }, 10000);
   });
 });
 
